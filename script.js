b14cde77c1737e645da5c82846e7b261f0eae6d5
AnisaElokAsmaransyah <106418871+AnisaElokAsmaransyah@users.noreply.github.com>
2022-07-05 06:57:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+h1 = new Typed("#typed", {
+    strings : ["Hallo saya, Anisa Elok Asmaransyah ",],
+    typedSpeed :100,
+    startDelay : 90,
+    loop : true
+});
+
+p = new Typed("#otomatis", {
+    strings : ["Berikut adalah tugas praktikum WEB yaitu membuat sebuah WEB portofolio"],
+    typedSpeed :40,
+    startDelay : 300,
+    loop : true
+});
